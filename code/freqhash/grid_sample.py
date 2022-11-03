1039561f9af25b2eab7cecc3d3204d23a3ac63c5
Jae Yong Lee <leejaeyong7@gmail.com>
2022-11-03 00:23:06 -0500

    no jit

--- code/freqhash/grid_sample.py
+++ code/freqhash/grid_sample.py
@@ -53,7 +53,6 @@ def grid_sample_3d(vol, sample):
 
     # FD x C
     return (fn * (1 - wz) + ff * wz).view(FD, -1, 1, N)
-@torch.jit.script
 def grid_sample_2d(mat, sample):
     # Nx2
     FD, C, RH, RW = mat.shape
@@ -87,7 +86,6 @@ def grid_sample_2d(mat, sample):
 
     # FD x C
     return (ft * (1 - wy) + fb * wy).view(FD, -1, 1, N)
-@torch.jit.script
 def grid_sample_1d(vec, sample):
     FD, C, H, _ = vec.shape
     _, _, N, _ = sample.shape
