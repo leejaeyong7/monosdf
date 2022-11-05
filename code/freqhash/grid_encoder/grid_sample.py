669dac81602479de742aa2e9182dc44e4a19e4a6
Jae Yong Lee <jae.lee@reconstructinc.com>
2022-11-05 00:07:18 -0500

    CUDA + LR =5

--- code/freqhash/grid_encoder/grid_sample.py
+++ code/freqhash/grid_encoder/grid_sample.py
@@ -51,8 +51,8 @@ def grid_sample_2d(mat, sample):
 def grid_sample(input, grid):
     if input.shape[-1] == 1:
         return grid_sample_1d(input, grid)
-    else:
-        return grid_sample_2d(input, grid)
+    # else:
+    #     return grid_sample_2d(input, grid)
     return grid_sample_cuda.apply(input, grid)
 
 class grid_sample_cuda(torch.autograd.Function):
