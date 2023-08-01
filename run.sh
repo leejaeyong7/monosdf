7e9833bdea3609c6df7e7bed7e4f17a8e9b7fe6d
Jae Yong Lee <leejaeyong7@gmail.com>
2023-08-01 02:06:11 -0500

    updating run.sh

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+MOUNT_DIR="/home/lee896/data/$(hostname -s)"
+DATA_DIR="${MOUNT_DIR}/monosdf"
+EXP_DIR="${MOUNT_DIR}/monosdf-qff-results"
+run_name=""
+scene=$1
+qff_type=$2
+if [[ $2 = "0" ]]; then
+  run_name="scannet_mlp"
+  conf=scannet_mlp.conf
+else
+  run_name="scannet_qff_${qff_type}"
+  conf=scannet_qff_${qff_type}.conf
+fi
+
+cd code
+python training/exp_runner.py --conf confs/${conf} --scan_id $scene --data_folder=${DATA_DIR} --exps_folder=${EXP_DIR} --timestamp=server
+
+# zip files
+cd ${EXP_DIR}
+zip -r "${run_name}_${scene}.zip" "${run_name}_${scene}/server/surfaces"
+
+
+result_dirname="monosdf-qff-timing"
+ssh jyl "mkdir -p /mnt/data1/cluster_results/${result_dirname}/${run_name}"
+ssh jyl "mkdir -p /mnt/data1/monosdf_outputs/${result_dirname}/${run_name}/${scene}"
+scp "${run_name}_${scene}.zip" jyl:/mnt/data1/cluster_results/${result_dirname}/${run_name}/
+ssh jyl "cd /mnt/data1/cluster_results/${result_dirname}/${run_name}/ && unzip "{run_name}_${scene}.zip" -d /mnt/data1/monosdf_outputs/${result_dirname}/${run_name}/${scene}"
+
