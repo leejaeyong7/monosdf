250330881807c30fbbba1111140390e3c7cb1fbd
Jae Yong Lee <leejaeyong7@gmail.com>
2023-08-01 01:40:36 -0500

    removing dependency for cuda

--- code/freqhash/grid_encoder/grid_sample.py
+++ code/freqhash/grid_encoder/grid_sample.py
@@ -1,5 +1,5 @@
 import torch
-from .grid_backward import grid_backward
+# from .grid_backward import grid_backward
 
 # def grid_sample(input, grid):
 #     return grid_sample_cuda.apply(input, grid)
@@ -55,20 +55,20 @@ def grid_sample(input, grid):
         return grid_sample_2d(input, grid)
     # return grid_sample_cuda.apply(input, grid)
 
-class grid_sample_cuda(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input, grid):
-        assert input.ndim == 4
-        assert grid.ndim == 4
-        output = torch.nn.functional.grid_sample(input=input, grid=grid, mode='bilinear', align_corners=True)
-        ctx.save_for_backward(input, grid)
-        return output
+# class grid_sample_cuda(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx, input, grid):
+#         assert input.ndim == 4
+#         assert grid.ndim == 4
+#         output = torch.nn.functional.grid_sample(input=input, grid=grid, mode='bilinear', align_corners=True)
+#         ctx.save_for_backward(input, grid)
+#         return output
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, grid = ctx.saved_tensors
-        grad_input, grad_grid = grid_backward.apply(grad_output, input, grid)
-        return grad_input, grad_grid
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         input, grid = ctx.saved_tensors
+#         grad_input, grad_grid = grid_backward.apply(grad_output, input, grid)
+#         return grad_input, grad_grid
 
 # import torch
 # from .grid_backward import grid_backward
